56625b48ad396ea9ab7865c0536141f90f84e034
Александр <nikol.alex06@mail.ru>
2025-03-30 00:24:09 +0300

    feat(frontend): add table vision for tasks list and unite filter for both variants of visions

---
+++ frontend/src/components/tasks/components/data-table-row-actions.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Row } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Task, taskSchema } from "@/api/get-tasks";
+
+interface DataTableRowActionsProps {
+  row: Row<Task>;
+  onEdit: () => void;
+  onDelete: () => void;
+}
+
+export function DataTableRowActions({
+  row,
+  onEdit,
+  onDelete,
+}: DataTableRowActionsProps) {
+  taskSchema.parse(row.original);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="data-[state=open]:bg-muted flex h-8 w-8 p-0"
+        >
+          <MoreHorizontal />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[160px]">
+        <DropdownMenuItem onClick={onEdit}>Edit</DropdownMenuItem>
+        <DropdownMenuItem onClick={onDelete}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ frontend/src/components/tasks/components/data-table.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { ColumnDef, flexRender } from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { DataTablePagination } from "./data-table-pagination";
+
+interface TasksTableProps<TValue> {
+  columns: ColumnDef<Task, TValue>[];
+  table: any;
+}
+
+export function TasksTable<TValue>({
+  columns,
+  table,
+}: TasksTableProps<TValue>) {
+  return (
+    <>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className={`${header.id == "title" && "pl-6"}`}
+                      colSpan={header.colSpan}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <>
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </>
+  );
+}
