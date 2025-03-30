e496a3824a9f5683564695b0d4bd09e787d168de
Kirill Podkovyrin <kyrimer2010@gmail.com>
2025-03-30 04:19:40 +0300

    fix(backend): bug about fake emails

--- backend/src/main/java/com/jellyone/adapters/mail/SenderService.java
+++ backend/src/main/java/com/jellyone/adapters/mail/SenderService.java
@@ -1,11 +1,5 @@
 package com.jellyone.adapters.mail;
 
-import com.jellyone.mail.dto.TaskDTO;
-import org.thymeleaf.context.Context;
-
-import java.io.File;
-import java.util.List;
-
 public interface SenderService {
     void sendMail(String email, Long userId, Long eventId);
     void sendMail(Long userId, Long eventId);
