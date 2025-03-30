d41bccf28f5a74c93562733b2775f57d6946f969
forafox <deskpa@yandex.ru>
2025-03-30 12:01:57 +0300

    feat(backend): implementations of MailApi

--- backend/src/main/java/com/jellyone/adapters/mail/MailAdapter.java
+++ backend/src/main/java/com/jellyone/adapters/mail/MailAdapter.java
@@ -1,35 +1,45 @@
 package com.jellyone.adapters.mail;
 
+import com.jellyone.domain.Task;
 import com.jellyone.mail.api.MailApi;
 import com.jellyone.mail.dto.UserDTO;
+import com.jellyone.service.EventService;
+import com.jellyone.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Profile("mail")
 public class MailAdapter implements MailApi {
 
+    private final EventService eventService;
+    private final TaskService taskService;
+
     @Override
     public double getTotalSum(Long eventId) {
-        return 0.53453;
+        List<Task> tasks = taskService.getAllByEventID(eventId);
+        return tasks.stream().mapToDouble(Task::getExpenses).sum();
     }
 
     @Override
     public double getSumOwedToUser(Long eventId, Long userId) {
-        return -1.2322;
+        return 1235;
     }
 
     @Override
     public double getSumSpentByUser(Long eventId, Long userId) {
-        return -0.23232;
-    }
-
-    @Override
-    public HashMap<UserDTO, Double> getUserDebts(Long eventId, Long userId) {
-        return null;
+        List<Task> tasks = taskService.getAllByEventID(eventId);
+        double sum = 0;
+        for (Task task : tasks) {
+            if (task.getAssignee() != null && task.getAssignee().getId().equals(userId)) {
+                sum += task.getExpenses();
+            }
+        }
+        return sum;
     }
 }
--- backend/src/main/java/com/jellyone/repository/TaskRepository.java
+++ backend/src/main/java/com/jellyone/repository/TaskRepository.java
@@ -8,15 +8,19 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
     @Query(
             value = """
-            SELECT * 
-            FROM tasks t
-            WHERE t.event_id = :eventId
-            """,
+                    SELECT * 
+                    FROM tasks t
+                    WHERE t.event_id = :eventId
+                    """,
             nativeQuery = true
     )
     Page<Task> findAllByEventId(@Param("eventId") Long eventId, Pageable pageable);
+
+    List<Task> findAllByEventId(Long eventId);
 }
